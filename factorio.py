12dabf28688298694a2f1897753a42f1aa0b7ef6
Liam <liam@liquorice.com.au>
2019-08-30 13:26:49 +1000

    Add machine options to include more recipes

--- factorio.py
+++ factorio.py
@@ -12,16 +12,16 @@ sciences = [
 ]
 output = 1
 
-CRAFT_SPEED = 0.75
 PROD = 1
 
 
-def calc_input_required(item, output_per_sec, prod=PROD, speed=CRAFT_SPEED):
+def calc_input_required(item, output_per_sec, prod=PROD):
     if item in recipes:
         recipe = recipes[item]
+        speed = recipe["machine"]["speed"]
         craft_output = recipe["output"] * prod / (recipe["craft_time"] / speed)
         assemblers = output_per_sec / craft_output
-        print(f"- {assemblers} assemblers for {item}")
+        print(f"- {assemblers} {recipe['machine']['name']}s for {item}")
 
         for input, amount in recipe["inputs"].items():
             input_required = assemblers * amount / (recipe["craft_time"] / speed)
